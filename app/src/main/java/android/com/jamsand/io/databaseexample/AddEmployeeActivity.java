1efddf4c46161af126af257158be17246f7aaf10
odwa <odwa.menziwa@jumpco.io>
2023-01-12 16:20:41 +0200

    created AddEmployee screen

---
+++ app/src/main/java/android/com/jamsand/io/databaseexample/AddEmployeeActivity.java
@@ -0,0 +1,18 @@
+package android.com.jamsand.io.databaseexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee);
+    }
+
+    public void addButtonPressed(View view) {
+    }
+}
--- app/src/main/java/android/com/jamsand/io/databaseexample/MainActivity.java
+++ app/src/main/java/android/com/jamsand/io/databaseexample/MainActivity.java
@@ -1,14 +1,38 @@
 package android.com.jamsand.io.databaseexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseHelper myHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_emp_list);
+
+        myHelper = new DatabaseHelper(this);
+        myHelper.open();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_record) {
+            Intent add_mem = new Intent(this,AddEmployeeActivity.class);
+            startActivity(add_mem);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
